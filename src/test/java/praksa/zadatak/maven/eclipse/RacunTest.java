330d74812276a8661c8a731515116f2b5eacd306
Nikola Kulas <95760016+Dzoni29@users.noreply.github.com>
2023-08-07 15:43:51 +0200

    Jos testova

--- src/test/java/praksa/zadatak/maven/eclipse/RacunTest.java
+++ src/test/java/praksa/zadatak/maven/eclipse/RacunTest.java
@@ -8,16 +8,32 @@ import org.junit.jupiter.api.Test;
 
 public class RacunTest {
 	
-	private Racun racun;
-	
-	@BeforeEach
-	public void setUp() {
-		racun = new Racun("12345", 1000, "HRK");
-	}
-	
+    private Racun racun;
+
+    @BeforeEach
+    public void setUp() {
+        racun = new Racun("12345", 1000, "HRK");
+    }
+
     @Test
     public void testGetBrojRacuna() {
         assertEquals("12345", racun.getBrojRacuna());
+    }
+
+    @Test
+    public void testGetIznos() {
+        assertEquals(1000, racun.getIznos(), 0.001);
+    }
+
+    @Test
+    public void testGetValuta() {
+        assertEquals("HRK", racun.getValuta());
+    }
+
+    @Test
+    public void testSmanjiIznos() {
+        racun.smanjiIznos(500);
+        assertEquals(500, racun.getIznos(), 0.001);
     }
 	
 }
