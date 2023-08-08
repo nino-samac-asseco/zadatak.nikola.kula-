1633effcb9ad7e7f6108b80db4e3cb1af1cde095
Nikola Kulas <95760016+Dzoni29@users.noreply.github.com>
2023-08-08 19:12:24 +0200

    log4j

--- src/main/java/praksa/zadatak/maven/eclipse/Main.java
+++ src/main/java/praksa/zadatak/maven/eclipse/Main.java
@@ -1,15 +1,15 @@
 package praksa.zadatak.maven.eclipse;
 
-import java.util.Scanner;
-
-import  praksa.zadatak.maven.eclipse.Menu;
-import  praksa.zadatak.maven.eclipse.Prijava;
-import  praksa.zadatak.maven.eclipse.Racun;
-import  praksa.zadatak.maven.eclipse.TransakcijaLista;
+import java.util.Scanner; 
+import org.apache.log4j.Logger;  
 
 public class Main {
 	
+    private static final Logger logger = Logger.getLogger(Main.class);
+    
 	public static void main(String[] args) {
+
+		logger.info("Zapocet program");
 		
 		Scanner scanner = new Scanner(System.in);
 		
@@ -21,10 +21,14 @@ public class Main {
 				TransakcijaLista transakcijaLista = new TransakcijaLista();
 				Menu.showMenu(scanner, stanjeRacuna, transakcijaLista);
 				
+			} else {
+				logger.warn("Neuspjela prijava");
 			}
 		}
 		
 		scanner.close();
 		
+        logger.info("Program zavrsen");
+        
 	}
 }
--- src/main/java/praksa/zadatak/maven/eclipse/Prijava.java
+++ src/main/java/praksa/zadatak/maven/eclipse/Prijava.java
@@ -1,14 +1,20 @@
 package praksa.zadatak.maven.eclipse;
 
 import java.util.Scanner;
+import org.apache.log4j.Logger;  
 
-import praksa.zadatak.maven.eclipse.MD5Util;
+
 
 public class Prijava {
+	
+    private static final Logger logger = Logger.getLogger(Prijava.class);
+
+	
 	private static final String USERNAME = "johndoe";
 	private static final String PASSWORD = "12345abcde";
 	
 	public static boolean login(Scanner scanner) {
+		
 		System.out.println("Prijava u aplikaciju");
 		System.out.print("Username: ");
 		String username = scanner.nextLine();
@@ -17,6 +23,7 @@ public class Prijava {
 		
 		if (username.equals(USERNAME) && MD5Util.encrypt(password).equals(MD5Util.encrypt(PASSWORD))) {
 			System.out.println("Prijava uspjesna!");
+			logger.info("Korisnik " + USERNAME + " uspjesno se prijavio.");
 			return true;
 		} else {
 			System.out.println("Netocan username ili password");
